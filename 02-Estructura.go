45795c2404abc9a7354304c4b0885a3a739fd54e
Juan Morales <juanmr2311@gmail.com>
2025-03-22 18:24:58 -0500

    feat: Se agregan mas tipos de datos

--- 02-Estructura.go
+++ 02-Estructura.go
@@ -42,6 +42,24 @@ listaPaises := [3] int
 var numeros []int = []int {datos}
 se utilizan los métodos para agregar datos, por ejemplo el append
 numeros = append(numeros, 6)
+
+>> Mapas tipo de arreglo que representa una colección de clave valor. Donde cada clave es única y el valor puede ser cualquier
+tipo de dato, son conocidos en otros lenguajes como arreglos asociativos, diccionarios, hashmaps, etc...
+var edades map[string]int = map[string]int{"Juan": 27, "Sarah", 27 }
+
+>> Struct
+Este tipo de datos representa un conjunto de campos con diferentes tipos de datos.
+Las estructuras se utilizan para agrupar diferentes tipos de datos en una única variable.
+// Declarar una estructura con diferentes campos
+type Persona struct {
+   nombre string
+   edad int
+   altura float64
+}
+
+var persona1 Persona = Persona{"Juan", 28, 1.75} //Son como las clases en Java, y esta sería la forma de instanciar
+
+Se profundizará en los structs mas adelante...
 */
 
 package main
@@ -76,5 +94,24 @@ func func02() {
 	//Rango no definido [1:]
 	//Rango inverso [:5]
 	numeros2 := numeros[2:5]
+
+	fmt.Println(numeros)
+	fmt.Println(numeros2)
+
+	capitales := map[string]string{
+		"Colombia":  "Bogotá",
+		"Chile":     "Santiago",
+		"Venezuela": "Caracas",
+	}
+	//Podemos agregar más propiedades y su valor
+	capitales["Argentina"] = "Buenos Aires"
+	//Podemos quitar también elementos del mapa
+	delete(capitales, "Venezuela")
+
+	//Si se le pide un valor no definido va a mostrar un valor vacío
+	fmt.Println(capitales)
+	fmt.Println("Capital de colombia : ", capitales["Colombia"])
+	fmt.Println("Agregando la capital de Argentina: ", capitales)
+	fmt.Println("Quitando la capital de Venezuela: ", capitales)
 
 }
